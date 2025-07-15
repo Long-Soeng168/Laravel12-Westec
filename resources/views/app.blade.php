14379026669597c8e6e720bdf1fd8f9f6747e8e5
Long-Soeng167 <generalpurpose167@gmail.com>
2025-07-15 09:22:17 +0700

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,6 +32,7 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <meta name="robots" content="index, follow">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
